d40c75247e0a992e3bc8906a9564314be4272f62
Kenneth Gregory <kenneth.gregory@cryptotho.com>
2025-11-03 17:01:21 -0600

    Beginning of class 4

--- app.py
+++ app.py
@@ -4,20 +4,68 @@ import os
 from tqdm import tqdm
 import psutil
 from halo import Halo
+import sys
+import atexit
+import keyboard
+from io import StringIO
 def main():
+    
+# Save the original stdout
 
+    
+# Create a StringIO object to capture the output
 
-    print("Welcome to CompSci 150 Malware Scanner")
-    print(" ")
-
-    user_response = input('''
-          Welcome, What would you like to do? Enter the number for your choice.
+    
+    global saved_state_flag
+    global high_water_mark_file
+    high_water_mark_file = " " 
+    saved_state_flag = False
+    atexit.register(save_progress_on_exit)
+    message1 ='''
+        Welcome, What would you like to do? Enter the number for your choice.
                           
         1) Scan the file system.
         2) Scan the active running processes.
         3) Scan the entire system.
+
+'''
+    message2 = '''
+
+         Welcome, What would you like to do? Enter the number for your choice.
                           
-                        ''')
+        1) Scan the file system.
+        2) Scan the active running processes.
+        3) Scan the entire system.
+        4) Continue with an in progress scan                 
+                          
+                           
+                             '''
+    if os.path.exists('scan_saved_progress_file'):
+        with open("scan_saved_progress_file", "r") as f:
+            high_water_mark_file = f.readline()
+        greet_user(message2)
+    else:
+         greet_user(message1)
+    
+    
+def greet_user(message):    
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print("Welcome to CompSci 150 Malware Scanner")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    
+    user_response = input(message)
+     
+                    
     if user_response == '1':
             scan_file_system()
     elif user_response == '2':
@@ -25,16 +73,31 @@ def main():
     elif user_response == '3':
             scan_file_system()
             scan_processes()
+    elif user_response == '4':
+           saved_state_flag = True
+           scan_file_system()
     else:
             print("That selection does not exist")
 
 
 def scan_file_system():
-    with Halo(text="Gathering file system file data", spinner='dots', text_color = 'magenta', animation = 'bounce'):
+    global saved_state_flag
+         
+    with Halo(text="Gathering file system file data. Once scanning begins, press escape to exit.", spinner='dots', text_color = 'magenta', animation = 'bounce'):
         compiled_rules = util.get_compiled_rules()
-        files_to_scan = tqdm(util.get_all_files())
+        files_to_scan = util.get_all_files()
+        if saved_state_flag == True:
+             if high_water_mark_file in files_to_scan:
+                  high_water_mark_index = files_to_scan.index(high_water_mark_file)
+                  temporary_file_list = files_to_scan[high_water_mark_index:]
+                  files_to_scan = temporary_file_list
+                  saved_state_flag = False
+        files_to_scan = tqdm(files_to_scan)
     matches = []
     for file in files_to_scan:
+        high_water_mark_file = file
+        if keyboard.is_pressed('esc'):
+             sys.exit()
         base_file = os.path.basename(file)
         message = "Scanning file: " + str(base_file[:15]).ljust(20)
         files_to_scan.set_description(message)
@@ -49,11 +112,18 @@ def scan_file_system():
             print(str(item))
 
 
+
 def scan_processes():
     compiled_rules = util.get_compiled_rules()
     pids = util.get_process_pids()
     for id in tqdm(pids):
             matches = compiled_rules.match(pid=id)
+
+def save_progress_on_exit():
+
+    line_to_write = high_water_mark_file
+    with open("scan_saved_progress_file", "w") as f:
+         f.write(line_to_write)
 
 
 def error_handler(message):
